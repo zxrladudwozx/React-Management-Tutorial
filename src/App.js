670edd8bb53c5018405980acf47fc76d0e98a9ec
zxrladudwozx <zxrladudwozx@gmail.com>
2019-12-29 20:48:42 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background"> 
+      <img src={logo} lat="logo"/>
+      <h2>asdf</h2>conative
+    </div>
+  );
+}
+
+export default App;
