1c793537a14da3388f45b53d8a16e3378d0a6306
zxrladudwozx <zxrladudwozx@gmail.com>
2019-12-30 19:38:38 +0900

    Add Prograssing bar

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,9 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import {withStyles} from '@material-ui/core/styles';
+
 //머테리얼 ui 는 react에서 강하다.
 
 const styles = theme =>({
@@ -19,18 +21,34 @@ const styles = theme =>({
   },
   table:{
     minWidth:1080
+  },
+  progress:{
+    margin:theme.spacing.unit*2
   }
 })
+/* React 라이브러리가 처음 컴포넌트를 실행할 때의 순서
+  1) constructor()
+  2) componentWillMount()
+  3) render()
+  4) componentDidMount()
 
+  그 이후
+  props or state가 변경되는 경우 => shouldComponentUpdate()
+  
+  React Component Life Cycle~
+*/
 class App extends Component{
 
   state={ //state는 컴포넌트 안에서 변경될 수 있는 데이터를 사용할 때 사용.
-    customers:""
+    customers:"",
+    completed:0
   }
 
   componentDidMount(){  //api서버에 접근
+    this.timer=setInterval(this.progress, 20);  //0.02초마다 시행되게끔 만듬
+    //callApi부분 3줄을 주석 처리하면 프로그래스 바를 볼 수 있음.
     this.callApi()
-    .then(res=>this.setState({customers:res}))
+    .then(res=>this.setState({customers:res}))  //상태가 변경됨.
     .catch(err=>console.log(err));
   }
 
@@ -38,6 +56,11 @@ class App extends Component{
     const response = await fetch('/api/customers');
     const body= await response.json();
     return body;
+  }
+
+  progress=()=>{
+    const {completed} = this.state;
+    this.setState({completed:completed>=100?0:completed+1});  //0~100까지의 값을 왔다갔다 하게 됨.
   }
 
   render(){
@@ -69,8 +92,14 @@ class App extends Component{
                   job={c.job}
                 />
               )
-            })//비동기적 방식의 단점. 맨 처음에는 값이 없다고 나오니 그 값이 없다면 공백값을 넣어줌.
-          :""} 
+            }) ://비동기적 방식의 단점. 맨 처음에는 값이 없다고 나오니 그 값이 없다면 프로그래스 바 생김
+          <TableRow>
+            <TableCell colSpan="6" align="center">
+              <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+            </TableCell>
+          </TableRow>
+
+          } 
           </TableBody>
         </Table>
       </Paper>
